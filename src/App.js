6cb3eb03becb760d56045033c637620e074b9e1e
kasunharshana352 <139376547+kasunharshana352@users.noreply.github.com>
2025-07-05 23:40:25 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from './components/Home';
-import './App.css'; // Add CSS for layout adjustments
+import Projects from "./components/Projects";
+import ProjectDetails from "./components/ProjectDetails";
+import AboutUs from "./components/AboutUs";
+import ContactUs from "./components/ContactUs";
+import NewsMedia from "./components/NewsMedia";
+import NewsArticleDetail from "./components/NewsArticleDetail"; 
+import './App.css';
 
 function App() {
   return (
@@ -13,6 +19,12 @@ function App() {
         <div className="main-content">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/projects/:id" element={<ProjectDetails />} />
+            <Route path="/about-us" element={<AboutUs />} />
+            <Route path="/contact-us" element={<ContactUs/>} />
+            <Route path="/news-media" element={<NewsMedia/>} />
+            <Route path="/news-media/article/:id" element={<NewsArticleDetail/>} /> {/* Add this route */}
           </Routes>
         </div>
         <Footer />
